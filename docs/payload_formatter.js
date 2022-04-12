333401060f485fc011f299279811f674d71a38e7
Tomáš Silber <silber.tomas@outlook.cz>
2022-04-12 20:09:29 +0200

    Update payload_formatter and node-red flow

--- docs/payload_formatter.js
+++ docs/payload_formatter.js
@@ -1,5 +1,4 @@
-function sflt162f(rawSflt16)
-    {
+function sflt162f(rawSflt16) {
     // rawSflt16 is the 2-byte number decoded from wherever;
     // it's in range 0..0xFFFF
     // bit 15 is the sign bit
@@ -38,51 +37,50 @@ function sflt162f(rawSflt16)
     var f_unscaled = sSign * mant1 * Math.pow(2, exp1 - 15);
 
     return f_unscaled;
-    }
+}
 
 function Decoder(bytes, port) {
-  //Data example: 596FCD77C7566A66136D736D1E5D606C9C6E1D6F3A6F2C54
-  var decoded = {};
-  decoded.bytes = bytes;
+    var decoded = {};
+    decoded.bytes = bytes;
 
-  //Temperature and humidity
-  rawTemp = bytes[0] + bytes[1] * 256;
-  decoded.degreesC = sflt162f(rawTemp) * 100;
+    //Temperature and humidity
+    rawTemp = bytes[0] + bytes[1] * 256;
+    decoded.temperature = sflt162f(rawTemp) * 100;
 
-  rawHumid = bytes[2] + bytes[3] * 256;
-  decoded.humidity = sflt162f(rawHumid) * 100;
+    rawHumid = bytes[2] + bytes[3] * 256;
+    decoded.humidity = sflt162f(rawHumid) * 100;
 
-  //Particle mass concontration
-  pm1 = bytes[4] + bytes[5] * 256;
-  decoded.pm1 = sflt162f(pm1) * 100;
+    //Particle mass concontration
+    pm1p0 = bytes[4] + bytes[5] * 256;
+    decoded.pm1p0 = sflt162f(pm1p0) * 100;
 
-  pm2 = bytes[6] + bytes[7] * 256;
-  decoded.pm2 = sflt162f(pm2) * 100;
+    pm2p5 = bytes[6] + bytes[7] * 256;
+    decoded.pm2p5 = sflt162f(pm2p5) * 100;
 
-  pm4 = bytes[8] + bytes[9] * 256;
-  decoded.pm4 = sflt162f(pm4) * 100;
+    pm4p0 = bytes[8] + bytes[9] * 256;
+    decoded.pm4p0 = sflt162f(pm4p0) * 100;
 
-  pm10 = bytes[10] + bytes[11] * 256;
-  decoded.pm10 = sflt162f(pm10) * 100;
+    pm10p0 = bytes[10] + bytes[11] * 256;
+    decoded.pm10p0 = sflt162f(pm10p0) * 100;
 
-  //Particle number concontration
-  nc05 = bytes[12] + bytes[13] * 256;
-  decoded.nc05 = sflt162f(nc05) * 100;
+    //Particle number concontration
+    nc0p5 = bytes[12] + bytes[13] * 256;
+    decoded.nc0p5 = sflt162f(nc0p5) * 100;
 
-  nc10 = bytes[14] + bytes[15] * 256;
-  decoded.nc10 = sflt162f(nc10) * 100;
+    nc1p0 = bytes[14] + bytes[15] * 256;
+    decoded.nc1p0 = sflt162f(nc1p0) * 100;
 
-  nc25 = bytes[16] + bytes[17] * 256;
-  decoded.nc25 = sflt162f(nc25) * 100;
+    nc2p5 = bytes[16] + bytes[17] * 256;
+    decoded.nc2p5 = sflt162f(nc2p5) * 100;
 
-  nc40 = bytes[18] + bytes[19] * 256;
-  decoded.nc40 = sflt162f(nc40) * 100;
+    nc4p0 = bytes[18] + bytes[19] * 256;
+    decoded.nc4p0 = sflt162f(nc4p0) * 100;
 
-  nc10p0 = bytes[20] + bytes[21] * 256;
-  decoded.nc10p0 = sflt162f(nc10p0) * 100;
+    nc10p0 = bytes[20] + bytes[21] * 256;
+    decoded.nc10p0 = sflt162f(nc10p0) * 100;
 
-  typical = bytes[22] + bytes[23] * 256;
-  decoded.typical = sflt162f(typical) * 100;
+    typical = bytes[22] + bytes[23] * 256;
+    decoded.typical_value = sflt162f(typical) * 100;
 
-  return decoded;
+    return decoded;
 }
