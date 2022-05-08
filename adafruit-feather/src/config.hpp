5a869cc18639fbf5798d14f04df15afb35f9d3e5
Tomáš Silber <silber.tomas@outlook.cz>
2022-05-09 00:28:26 +0200

    Add comments

--- adafruit-feather/src/config.hpp
+++ adafruit-feather/src/config.hpp
@@ -1,16 +1,44 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
-//For debugging purposes define -D DEBUG=1 in platform.io or create macro here
+/** \file config.hpp
+ * This file holds config values such as server ip, delay values, pin values etc.
+ */
 
-#define DEBUG 1 // Enable or disable Serial prints
+//
+
+/** @cond */
+/************************************************
+ *               Configuration
+ ***********************************************/
+/** @endcond */
+
+/**< Enable or disable Serial prints. For debugging purposes define -D DEBUG=1 in platform.io or create macro here */
+#define DEBUG 0
+
+/**< SPS30 cleanign interval in days. */
 #define SPS30_CLEAN_INTERVAL_IN_DAYS 7
-#define TX_TIMER_SECONDS 60 // Default time for data transmission interval in seconds
-#define PAYLOAD_BUFFER_SIZE 25 // Payload(array containing data) buffer size
-#define SERIAL_SPEED 9600 // Serial communication speed
-#define MINIMUM_ALLOWED_TX_TIMER_IN_SECONDS 60 // Minimum allowed interval for speed transmission -> do not set to too low, you must respect regional transmission time
-#define MAXIMUM_ALLOWED_TX_TIMER_IN_SECONDS 604800 //Minimum allowed interval for speed transmission, default value is 604800 == 1 week
 
+/**< Default time for data transmission interval in seconds. */
+#define TX_TIMER_SECONDS 60
+
+/**< Payload(array containing data) buffer size. */
+#define PAYLOAD_BUFFER_SIZE 25
+
+/**< Serial communication speed. */
+#define SERIAL_SPEED 9600
+
+/**< Minimum allowed interval for speed transmission -> do not set to too low, you must respect regional transmission time. */
+#define MINIMUM_ALLOWED_TX_TIMER_IN_SECONDS 60
+
+/**< Minimum allowed interval for speed transmission, default value is 604800 == 1 week. */
+#define MAXIMUM_ALLOWED_TX_TIMER_IN_SECONDS 604800
+
+/** @cond */
+/************************************************
+ *               Debug macros
+ ***********************************************/
+/** @endcond */
 #if DEBUG == 1
   #define DBG_SERIAL_BEGIN(x) Serial.begin(x);
   #define DBG_PRINTF(x, y) Serial.printf(x, y);
